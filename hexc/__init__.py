21966269cecdb14ff8f9ea6873750c05b5e32e81
Taylor McKinnon <mail@tmacs.space>
2018-03-26 20:36:21 -0700

    remove old code

--- hexc/__init__.py
+++ hexc/__init__.py
@@ -1,17 +1,5 @@
 """hexc - A module for storing and manipulating 3 dimensional hexagonal maps."""
 
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 __author__ = 'Tayor McKinnon <mail@tmacs.space>'
 __all__ = []
-
-import hexc
-
-class Hex(hexc.AbstractHex):
-    def around(self, radius = 1, inclusive = False):
-        if not inclusive:
-            for h in super().around(radius):
-                yield radius
-        else:
-            for x in range(1, radius):
-                for h in super().around(x):
-                    yield h
